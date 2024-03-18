58449403346a4ac3df942ebb721779d9ac7af1ea
jisellef <jiselle.f24@k12.xrds.org>
2024-03-18 11:38:37 -0700

    Updated Stuff

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/model')
+def model():
+    return render_template("model.html")
 
 if __name__ == '__main__':
-    app.run(debug=True,port=3000)
+    app.run(debug=True,port=5000)
--- static/sketch.js
+++ static/sketch.js
@@ -1,80 +1,90 @@
-// Copyright (c) 2019 ml5
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
+    // Classifier Variable
+    let classifier;
+    // Model URL
+    let imageModelURL = 'https://teachablemachine.withgoogle.com/models/BVaT94d0n/';
 
-/* ===
-ml5 Example
-Webcam Image Classification using a pre-trained customized model and p5.js
-This example uses p5 preload function to create the classifier
-=== */
+    // Video
+    let video;
+    let flippedVideo;
+    // To store the classification
+    let label = "";
 
-// Classifier Variable
-let classifier;
-// Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/JLmkJzpVE/';
+    // Load the model first
+    function preload() {
+      classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+    }
 
-// Video
-let video;
-let flippedVideo;
-// To store the classification
-let label = "";
+    function setup() {
+        var canvas = createCanvas(500, 450);
+        canvas.parent('sketch-holder');
+      video = createCapture(VIDEO);
+      video.size(500, 420);
+      video.hide();
 
-// Load the model first
-function preload() {
-  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
-}
+      flippedVideo = ml5.flipImage(video);
+      // Start classifying
+      classifyVideo();
+    }
 
-function setup() {
-  createCanvas(320, 260);
-  // Create the video
-  video = createCapture(VIDEO);
-  video.size(320, 240);
-  video.hide();
+    function draw() {
+      background(0);
+      // Draw the video
+      if (label === 'Blue Marker') {
+        background(0, 0, 255);
+        textSize(24);
+        textAlign(CENTER);
+        text("Blue Marker", width / 2, height - 20);
+      }
+        else if (label === 'Green Marker') {
+        background(0, 255, 0);
+        textSize(24);
+        textAlign(CENTER);
+        text("Green Marker", width / 2, height - 20);
+      }
+        else if (label === 'Red Marker') {
+        background(255, 0, 0);
+        textSize(24);
+        textAlign(CENTER);
+        text("Red Marker", width / 2, height - 20);
+      }
+        else if (label === 'yellow marker') {
+        background(255, 255, 0);
+        textSize(24);
+        textAlign(CENTER);
+        text("Yellow Marker", width / 2, height - 20);
+      }
+        else if (label === 'Orange Marker') {
+        background(255, 165, 0);
+        textSize(24);
+        textAlign(CENTER);
+        text("Orange Marker", width / 2, height - 20);
+      } else {
+        image(flippedVideo, 0, 0);
+      }
 
-  flippedVideo = ml5.flipImage(video)
-  // Start classifying
-  classifyVideo();
-}
+      // Draw the label
+      fill(255);
+      textSize(16);
+      textAlign(CENTER);
+      text(label, width / 2, height - 4);
+    }
 
-function draw() {
-  background(0);
-  // Draw the video
+    // Get a prediction for the current video frame
+    function classifyVideo() {
+      flippedVideo = ml5.flipImage(video)
+      classifier.classify(flippedVideo, gotResult);
+    }
 
-  if (label === 'wallet') {
-    background(random(255), random(255), random(255));
-    textSize(100);
-    textAlign(CENTER);
-    text("HIIII", width/2, height -4);
-  }
-  else{
-  image(flippedVideo, 0, 0);
-
-  // Draw the label
-  fill(255);
-  textSize(16);
-  textAlign(CENTER);
-  text(label, width / 2, height - 4);
-
-  }
-}
-
-// Get a prediction for the current video frame
-function classifyVideo() {
-  flippedVideo = ml5.flipImage(video)
-  classifier.classify(flippedVideo, gotResult);
-}
-
-// When we get a result
-function gotResult(error, results) {
-  // If there is an error
-  if (error) {
-    console.error(error);
-    return;
-  }
-  // The results are in an array ordered by confidence.
-  // console.log(results[0]);
-  label = results[0].label;
-  // Classifiy again!
-  classifyVideo();
-}
+    // When we get a result
+    function gotResult(error, results) {
+      // If there is an error
+      if (error) {
+        console.error(error);
+        return;
+      }
+      // The results are in an array ordered by confidence.
+      // console.log(results[0]);
+      label = results[0].label;
+      // Classify again!
+      classifyVideo();
+    }
